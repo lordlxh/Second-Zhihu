00ddf038e221c17247b9c293451f51f478824a27
lordlxh <82203191+lordlxh@users.noreply.github.com>
2021-06-17 21:41:46 +0800

    Revert "除推荐外的全部实现"

--- register.php
+++ register.php
@@ -1,55 +1,27 @@
-<?php  //注册
-	function safe_input($data){
-		$data=trim("$data");
-		$data=stripcslashes("$data");
-		$data=htmlspecialchars("$data");
-		return $data;
-	}
-	$connect=mysqli_connect("localhost","root","");
-	if(!$connect){
-		die('Could not connect: ' . mysqli_error($connect));
-	}
-	if(!mysqli_select_db($connect,"yongHu")){//如果数据库不存在
-		if(!mysqli_query($connect,"CREATE DATABASE yongHu")){//如果创建失败
-			die('Error creating database: ' . mysqli_error($connect));
-		}
-	}
-//数据库存在或不存在但创建成功
-	$data=array();$biao=mysqli_query($connect,"show tables;");
-	while($row=mysqli_fetch_assoc($biao)){
-		$data+=$row;
-	}unset($biao,$row);
-	if(!in_array(strtolower("zhangHao"), $data)){//如果账号列表不存在
-		$sql="CREATE TABLE zhangHao(
-		uid int NOT NULL AUTO_INCREMENT,
-		PRIMARY KEY(uid),
-		uname varchar(20),
-		pword varchar(15),
-		email varchar(20)
-		)";
-		if(!mysqli_query($connect,$sql)){die('Error creating table: ' . mysqli_error($connect));}
-	}unset($data);
-//列表存在,注册用户账号信息
-	$sign=1;
-	$uname=safe_input($_POST["uname"]);
-	$pword=safe_input($_POST["pword"]);
-	$email=safe_input($_POST["email"]);//var_dump($_POST["uname"]);
-	$data=array();$yongHuMing=mysqli_query($connect,"SELECT uname FROM zhangHao");
-	while ($row=mysqli_fetch_assoc($yongHuMing)) {
-		$data[]=$row['uname'];
-	}unset($yongHuMing,$row);//取出所有用户名
-	if(in_array($uname,$data)){unset($data);
-		echo "<script  language = 'javascript' type = 'text/javascript'>alert('用户名已存在'); window.location.href = 'http://localhost/secondzhihu/zhuce.html';</script>";
-		$sign=0;
-	}
-	else{//检测用户名，密码是否合法
-		if(strlen($uname)<5){echo "<script  language = 'javascript' type = 'text/javascript'>alert('用户名过短');></script>";$sign=0;}if(strlen($uname)>20){echo "<script  language = 'javascript' type = 'text/javascript'>alert('用户名过长');></script>";$sign=0;}
-		if(strlen($pword)<6){echo "<script  language = 'javascript' type = 'text/javascript'>alert('密码过短');></script>";$sign=0;}if (strlen($pword)>15){echo "<script  language = 'javascript' type = 'text/javascript'>alert('密码过长');></script>";$sign=0;}
-	}
-	if($sign==1){
-		$sql="INSERT INTO zhangHao (uname, pword, email) VALUES ('$uname', '$pword', '$email')";
-		if(!mysqli_query($connect,$sql)){die('Error creating zhangHao: ' . mysqli_error($connect));}
-	echo "<script  language = 'javascript' type = 'text/javascript'> window.location.href = 'http://localhost/secondzhihu/denglu.html';</script>";}
-	mysqli_close($connect);
+<?php
+//data/users/register.php
 
-?>
+#1.获取请求提交的数据
+    $uname=$_REQUEST["uname"];
+    //uname值就是前端页面中name属性的值
+    $upwd=$_REQUEST["upwd"];
+    //确认密码不用获取，获取一个密码就行
+    $email=$_REQUEST["email"];
+    $phone=$_REQUEST["phone"];
+    $gender=$_REQUEST["gender"];
+
+#2.连接到数据库
+    require("init.php");
+
+#3.拼sql语句并执行
+    $sql="insert into register(uname,upwd,email,phone,gender)values('$uname','$upwd','$email','$phone','$gender')";
+    //字段值 外面用双引号，里面用单引号
+    $result=mysqli_query($conn,$sql);
+    //执行sql语句
+
+#4.根据执行结果给出响应
+    if($result==true){ //函数返回值
+        echo "注册成功";
+    }else{
+        echo "注册失败";
+    };
--- 变色.js
+++ 变色.js
@@ -1,61 +1,16 @@
-function dianzan(id) {
-			if(/▲喜欢/.test(id.innerHTML)){
+function zt(id) {
+			if (id.innerHTML==="▲赞同") {
 				id.style.color="white";
 				//id.style.fontWeight="bold";
-				id.style.background="#0066FF";
-				var xmlhttp;
-
-			if (window.XMLHttpRequest)
-  			{// code for IE7+, Firefox, Chrome, Opera, Safari
-  				xmlhttp=new XMLHttpRequest();
-  				}
-
-		else
-  		{// code for IE6, IE5
-  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  			}
-  			xmlhttp.open("GET","add.php?qid="+id.id,true);
-  			//window.open("add.php?qid="+id.id);
-			xmlhttp.send();
-		xmlhttp.onreadystatechange=function()
-  		{
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    		{
-    		id.innerHTML="▲已赞"+xmlhttp.responseText;
-    		}
-  			}
-
+				id.innerHTML="▲已赞";
+				id.style.background="#0066FF"
 			}
-
-
 			else {
 				id.style.color="#0066FF";
 				//id.style.fontWeight="bold";
-				id.innerHTML="";
+				id.innerHTML="▲赞同";
 				id.style.background="#E5EFFF"
-				var xmlhttp;
-
-			if (window.XMLHttpRequest)
-  			{// code for IE7+, Firefox, Chrome, Opera, Safari
-  				xmlhttp=new XMLHttpRequest();
-  				}
-  				
-		else
-  		{// code for IE6, IE5
-  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  			}
-  			xmlhttp.open("GET","minus.php?qid="+id.id,true);
-  			//window.open("add.php?qid="+id.id);
-			xmlhttp.send();
-		xmlhttp.onreadystatechange=function()
-  		{
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    		{
-    		id.innerHTML="▲喜欢"+xmlhttp.responseText;
-    		}
-  			}
-
-			}
+			};
 
 				
 }
@@ -76,69 +31,4 @@ function c(id) {
 function cnt(a){
 	alert(getElementById("cntbdy"));
 	getElementById("cntbdy").style.display="none";
-	var user=getParamter("uname");
-	document.getElementById('commenters').value=user;
-}
-
-function zt(id){
-	
-			if(/▲喜欢/.test(id.innerHTML)){
-				id.style.color="white";
-				//id.style.fontWeight="bold";
-				id.style.background="#0066FF";
-				var xmlhttp;
-
-			if (window.XMLHttpRequest)
-  			{// code for IE7+, Firefox, Chrome, Opera, Safari
-  				xmlhttp=new XMLHttpRequest();
-  				}
-
-		else
-  		{// code for IE6, IE5
-  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  			}
-  			xmlhttp.open("GET","addc.php?cid="+id.id,true);
-  			//window.open("add.php?qid="+id.id);
-			xmlhttp.send();
-		xmlhttp.onreadystatechange=function()
-  		{
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    		{
-    		id.innerHTML="▲已赞"+xmlhttp.responseText;
-    		}
-  			}
-
-			}
-
-
-			else {
-				id.style.color="#0066FF";
-				//id.style.fontWeight="bold";
-				id.innerHTML="";
-				id.style.background="#E5EFFF"
-				var xmlhttp;
-
-			if (window.XMLHttpRequest)
-  			{// code for IE7+, Firefox, Chrome, Opera, Safari
-  				xmlhttp=new XMLHttpRequest();
-  				}
-  				
-		else
-  		{// code for IE6, IE5
-  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  			}
-  			xmlhttp.open("GET","minusc.php?cid="+id.id,true);
-  			//window.open("add.php?qid="+id.id);
-			xmlhttp.send();
-		xmlhttp.onreadystatechange=function()
-  		{
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    		{
-    		id.innerHTML="▲喜欢"+xmlhttp.responseText;
-    		}
-  			}
-
-			}
-
-				
 }
